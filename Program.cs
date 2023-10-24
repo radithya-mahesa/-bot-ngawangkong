74f854f1107d9774fa4d98fa3b374954b58f5b61
radithya-mahesa <radithya.syabil.com>
2023-10-24 21:15:34 +0700

    frst commit

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using CharacterAI_Discord_Bot.Service;
+using CharacterAI_Discord_Bot.Handlers;
+
+namespace CharacterAI_Discord_Bot
+{
+    public class Program : CommandsService
+    {
+        private ServiceProvider _services = null!;
+        private DiscordSocketClient _client = null!;
+
+        static void Main()
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
+        {
+            _services = CreateServices();
+            _client = _services.GetRequiredService<DiscordSocketClient>();
+
+            if (BotConfig is null) return;
+
+            _client.Log += Log;
+            _client.Ready += OnClientReady;
+            _client.JoinedGuild += OnGuildJoin;
+
+            await _client.LoginAsync(TokenType.Bot, BotConfig.BotToken);
+            await _client.StartAsync();
+            await _services.GetRequiredService<CommandsHandler>().InitializeAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task OnGuildJoin(SocketGuild guild)
+        {
+            await CreateBotRoleAsync(guild);
+            Success($"Joined guild: {guild.Name} | Owner: {guild.Owner.Username} | Members: {guild.MemberCount}");
+        }
+
+        public Task OnClientReady()
+        {
+            _ = Task.Run(async () => await LaunchChromeAndSetup(setup: BotConfig.AutoSetupEnabled));
+
+            return Task.CompletedTask;
+        }
+
+        private async Task LaunchChromeAndSetup(bool setup)
+        {
+            try
+            {
+                var handler = _services.GetRequiredService<CommandsHandler>();
+                await handler.CurrentIntegration.LaunchChromeAsync(BotConfig.CustomChromePath, BotConfig.CustomChromeExecPath);
+                if (setup) await AutoSetup(handler, _client);
+            }
+            catch (Exception e)
+            {
+                Failure(e.ToString(), client: _client);
+            }
+        }
+
+        private static ServiceProvider CreateServices()
+        {
+            var clientConfig = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.All
+                ^ GatewayIntents.GuildScheduledEvents
+                ^ GatewayIntents.GuildInvites
+                ^ GatewayIntents.GuildPresences,
+
+                MessageCacheSize = 5
+            };
+
+            var services = new ServiceCollection()
+                .AddSingleton(new DiscordSocketClient(clientConfig))
+                .AddSingleton(new CommandService())
+                .AddSingleton<CommandsHandler>();
+
+            return services.BuildServiceProvider();
+        }
+
+        private Task Log(LogMessage log)
+        {
+            if (log.Exception is NullReferenceException) return Task.CompletedTask;
+
+            Console.WriteLine(log.ToString());
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# CharacterAI Discord Bot
+An unofficial Discord integration with a [CharacterAI](https://beta.character.ai/) service which you can use to add any character on your own Discord server.
+
+<div align="center">
+    
+![Logo](https://user-images.githubusercontent.com/55811932/224168501-48e81f64-9b2f-442c-a8fe-6ecab8d7aab2.png)<br>
+<!-- ![Logo](https://user-images.githubusercontent.com/55811932/226441262-8edbb834-33d5-4cd2-8fac-0bacfa6ff79b.png) -->
+
+<sup><b>If you found this project useful, the best way to thank me is to simply leave a star ⭐<br>
+My [Discord Server](https://github.com/drizzle-mizzle/CharacterAI-Discord-Bot/discussions/22#discussioncomment-5502307) | Based on [CharacterAI.Net](https://github.com/drizzle-mizzle/CharacterAI.Net-API-Wrapper)</b></sup>
+</div>
+
+## 🪄Features
+- Talk with any character on your own server and change them on the fly
+- Embedded character search
+- Embedded DeepL translator
+- Filter "tails" - view the content of messages that were filtered and deleted (only the part that was written **before** the message was deleted, **it doesn't actually bypass the filter**)
+- Automatically sets the name and profile picture of selected character
+- Supports answers swiping <s>and image sending</s>broken, works only in one direction now (can receive images, but not send)
+- Supports multiple parallel chats with the same character
+- Random replies, audience mode and some other stuff, [read here](#documentation)
+
+    ![chrome_cshYMsGIaB](https://user-images.githubusercontent.com/55811932/211129383-c7cd4ca2-ceb4-42c5-8449-bc6ce9b2d538.gif)
+    
+## 📓Documentation
+- [How to set up](https://github.com/drizzle-mizzle/CharacterAI-Discord-Bot/wiki/How-to-set-up)
+- [Commands](https://github.com/drizzle-mizzle/CharacterAI-Discord-Bot/wiki/Commands)
+- [Additional configuration](https://github.com/drizzle-mizzle/CharacterAI-Discord-Bot/wiki/Additional-configuration)
+
+## 🔧Known issues
+- you can't send your pictures to a character, maybe will be fixed later (maybe not)
+- `private` command is broken again now
+
+    ![image](https://user-images.githubusercontent.com/55811932/236642586-d4d06f16-2016-4ec6-9481-995f9f251d61.png)
+
+## 🧱Build yourself
+- [.NET publishing guide](https://github.com/drizzle-mizzle/CharacterAI-Discord-Bot/wiki/Build-youself)
